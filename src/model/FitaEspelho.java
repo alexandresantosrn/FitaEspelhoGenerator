4221b37094993377e35053eb0e1b6680fe4dd903
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-02-12 12:14:47 -0300

    Implementando domínio

---
+++ src/model/FitaEspelho.java
@@ -0,0 +1,39 @@
+package model;
+
+public class FitaEspelho {
+
+	String codigo, sigla, mes, ano;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getMes() {
+		return mes;
+	}
+
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+}
